a9633dd3dbf33429a960a2512bdb87ede6e0108c
Avram-Karam <129803857+Avram-Karam@users.noreply.github.com>
2026-08-17 01:23:35 +0300

    Add files via upload

---
+++ assets/js/lectures-data.js
@@ -0,0 +1,31 @@
+/* محاضرات مؤتمر ميلادا عجيبًا */
+window.lectures = [
+  {
+    "id": "d1-l1",
+    "day": 1,
+    "title": "المحاضرة الأولى",
+    "speaker": "القمص منسي عزيز",
+    "speakerImg": "../assets/img/fr-mansi-aziz.jpg",
+    "time": "12:00 - 13:00",
+    "place": "القاعة الرئيسية",
+    "summary": "المحاضرة الروحية الأولى في مؤتمر ميلادا عجيبًا.",
+    "goals": [],
+    "keyTakeaways": [],
+    "notesEnabled": true,
+    "questions": []
+  },
+  {
+    "id": "d2-l1",
+    "day": 2,
+    "title": "المحاضرة الثانية",
+    "speaker": "القمص موريس حمدي",
+    "speakerImg": "../assets/img/fr-morris-hamdy.jpg",
+    "time": "11:30 - 12:30",
+    "place": "القاعة الرئيسية",
+    "summary": "المحاضرة الروحية الثانية في مؤتمر ميلادا عجيبًا.",
+    "goals": [],
+    "keyTakeaways": [],
+    "notesEnabled": true,
+    "questions": []
+  }
+];
